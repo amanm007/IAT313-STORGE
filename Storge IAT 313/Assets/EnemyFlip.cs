040b6a1bd83413e3452a68822a8dcca175d38ca1
amanm007 <95200325+amanm007@users.noreply.github.com>
2023-11-13 20:16:50 -0800

    Sprite Renderer for The beast
    flipping the sprite renderer according to the horizontal Movement

--- Storge IAT 313/Assets/EnemyFlip.cs
+++ Storge IAT 313/Assets/EnemyFlip.cs
@@ -17,11 +17,9 @@ public class EnemyFlip : MonoBehaviour
     {
         Vector2 currentPosition = transform.position;
 
-        // Check if the enemy has moved horizontally
+        
         if (currentPosition.x != lastPosition.x)
         {
-            // Flip the sprite based on the direction of movement
-            // If moving left (current position is less than last position), flipX is true
             spriteRenderer.flipX = currentPosition.x < lastPosition.x;
         }
 
